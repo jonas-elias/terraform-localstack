b35b57478ff276ca0dc21254a5206916c6786df8
Jonas Elias <jonasdasilvaelias@gmail.com>
2024-08-20 23:47:26 -0300

    feat: main tf t2 micro

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
